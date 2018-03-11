a68e614df678d9fd360303f8757edd5bdac7ca44
shoebkhan123 <34496751+shoebkhan123@users.noreply.github.com>
2018-03-11 21:20:03 +0530

    Add files via upload

---
+++ array/array_slice.php
@@ -0,0 +1,17 @@
+<?php
+/*
+In php has a built in functioin array_slice() that you can use to extract a range of elements from an array.
+
+*/
+
+$num = array(1,3,5,7,9);
+echo('<h1>Indexed Array slicing</h1><br>');
+print_r(array_slice($num, 0,3)); 
+
+?>
+
+
+
+
+
+
