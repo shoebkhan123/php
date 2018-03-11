e1bfde524ce7381f81f73e2f0de5d2030f713bc2
shoebkhan123 <34496751+shoebkhan123@users.noreply.github.com>
2018-03-11 21:01:21 +0530

    Add files via upload

---
+++ array/print_r.php
@@ -0,0 +1,22 @@
+<?php
+/*
+The count() function is used to return the length (the number of elements) of an array:
+
+
+*/
+
+$num = array(1,3,5,7,9);
+echo('<h1>Indexed Array</h1><br>');
+print_r($num);
+echo'<br>';
+echo('<h1>Associative Array</h1><br>');
+
+$info = array('name' => 'Shoeb', 'age' => 32, 'place' =>'Vellore');
+print_r($info);
+?>
+
+
+
+
+
+
